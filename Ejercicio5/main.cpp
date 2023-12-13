50394a6549b863e9646133f77b3a58118fa6bf20
ddealsan <ddealsan@myuax.com>
2023-12-13 10:32:19 +0100

    Avanzamos codigo

--- Ejercicio5/main.cpp
+++ Ejercicio5/main.cpp
@@ -8,3 +8,13 @@ private:
 
 public:
     Estudiante(std::string n, int e, float p) : nombre(n), edad(e), promedio(p) {}
+
+    void imprimir() {
+        std::cout << "Nombre: " << nombre << ", Edad: " << edad << ", Promedio: " << promedio << std::endl;
+    }
+
+    void modificarEdad(int nuevaEdad) {
+        edad = nuevaEdad;
+    }
+};
+
