3d4adf29796edc7b34d7dc5693bedad37ed1bdd9
ddealsan <ddealsan@myuax.com>
2023-12-13 10:28:21 +0100

    avanzamos codigo

--- Ejercicio3/main.cpp
+++ Ejercicio3/main.cpp
@@ -6,3 +6,11 @@ struct Estudiante {
     int edad;
     float promedio;
 };
+
+Estudiante copiarEstudiante(Estudiante est) {
+    Estudiante nuevoEst;
+    std::strcpy(nuevoEst.nombre, est.nombre);
+    nuevoEst.edad = est.edad;
+    nuevoEst.promedio = est.promedio;
+    return nuevoEst;
+}
