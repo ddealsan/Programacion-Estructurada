5b78c3c25b9855ea245ea0b9ed354284f39c5432
ddealsan <ddealsan@myuax.com>
2023-12-13 10:32:03 +0100

    Avanzamos codigo

--- Ejercicio5/main.cpp
+++ Ejercicio5/main.cpp
@@ -1,1 +1,10 @@
 #include <iostream>
+
+class Estudiante {
+private:
+    std::string nombre;
+    int edad;
+    float promedio;
+
+public:
+    Estudiante(std::string n, int e, float p) : nombre(n), edad(e), promedio(p) {}
