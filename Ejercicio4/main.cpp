2cba99e357606091deaf08fc592c5286ce7beb1c
ddealsan <ddealsan@myuax.com>
2023-12-13 10:30:31 +0100

    Avanzamos codigo

--- Ejercicio4/main.cpp
+++ Ejercicio4/main.cpp
@@ -6,3 +6,11 @@ typedef struct Estudiante {
     float promedio;
 } Estudiante;
 
+int main() {
+    Estudiante estudiante3;
+    std::strcpy(estudiante3.nombre, "Pedro");
+    estudiante3.edad = 25;
+    estudiante3.promedio = 9.7;
+
+    return 0;
+}
