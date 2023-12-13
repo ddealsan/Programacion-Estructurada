fa52c10daf03e1b40da518c225a6bceedd39b347
ddealsan <ddealsan@myuax.com>
2023-12-13 10:26:17 +0100

    Avanzamos codigo

--- Ejercicio2/main.cpp
+++ Ejercicio2/main.cpp
@@ -8,3 +8,12 @@ union Dato {
     float f;
     char str[20];
 };
+
+int main() {
+    Dato dato;
+
+    dato.i = 10;
+    printf("dato.i: %d\n", dato.i);
+
+    dato.f = 220.5;
+    printf("dato.f: %.2f\n", dato.f);
